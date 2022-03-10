2bda6b697b5ec5a5bf4c7bbf6467cd9065622e37
Thanasak <bestzacap20@gamail.com>
2022-03-10 13:37:41 +0700

    update 10/03/2022

--- restaurant-manage/database/connection.php
+++ restaurant-manage/database/connection.php
@@ -1,12 +1,12 @@
 <?php 
     $db_server = "localhost";
     $db_user = "root";
-    $db_password = "11121112";
+    $db_password = "";
     $db_name = "online_restaurant";
 
     $conn = mysqli_connect($db_server,$db_user,$db_password,$db_name);
     
-    if (mysqli_connect_errno($conn)){
+    if (!$conn){
         echo 'connection failed' . mysqli_connect_error();
         die();
     } else {
